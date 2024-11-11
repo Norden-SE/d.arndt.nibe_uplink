b7ae8b415fbf6e7b345170df595a3ce65d25cdbf
Norden-SE <dieter.arndt@me.com>
2024-11-11 08:28:52 +0100

    test

--- Nibe_F750/module.php
+++ Nibe_F750/module.php
@@ -6,7 +6,24 @@ class Nibe_F750 extends IPSModule {
     public function Create() {
         // Diese Zeile nicht löschen.
         parent::Create();
+
+        $this->RegisterVariableBoolean("ActionVariable", "Action Variable", "~Switch", 0);
+        $this->EnableAction("ActionVariable");
     }
+    
+    public function RequestAction($Ident, $Value) {
+        SetValue($this->GetIDForIdent($Ident), $Value);
+    }
+    
+    public function UpdateParameter($Field, $Parameter, $Value) {
+        $this->UpdateFormField($Field, $Parameter, $Value);
+    }
+
+    public function SetInvalid() {
+        $this->UpdateFormField('Label', 'caption', 'invalid'); // nicht ok
+    }
+    
+
     // Überschreibt die intere IPS_ApplyChanges($id) Funktion
     public function ApplyChanges() {
         // Diese Zeile nicht löschen
