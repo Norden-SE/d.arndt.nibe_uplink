6f064c391b27eb2bdb1116cf74f3ae9cef87e5a2
Norden-SE <dieter.arndt@me.com>
2024-11-11 13:12:24 +0100

    test

--- Nibe_F750/module.php
+++ Nibe_F750/module.php
@@ -7,7 +7,7 @@ class Nibe_F750 extends IPSModule {
         // Diese Zeile nicht löschen.
         parent::Create();
 
-        $this->RegisterVariableBoolean("Raumtemperatur", "Raumtemperatur", "", 0);
+        $this->RegisterVariableFloat("Raumtemperatur", "Raumtemperatur", "", 0);
         $this->EnableAction("ActionVariable");
     }
     
